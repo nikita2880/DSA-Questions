2a4d78c6e2fc04af19f271db13ba5e2b7a8d19c6
Nikita Sharma <nikita.sharmaa521@gmail.com>
2023-07-11 13:31:35 +0530

    Find duplicate in array

--- FindDuplicates.cpp
+++ FindDuplicates.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
 using namespace std;
 
+int findDuplicate ( int arr[], int size)
+{
+    int ans=0;
+    for ( int i=0; i<size ; i++)
+    {
+        ans=ans^arr[i];
+    }
+
+    for ( int i=1; i<size ; i++)
+    {
+        ans= ans^i;
+    }
+
+    return ans;
+}
+
 int main()
 {
-    
+    int n;
+    int arr[100];
+
+    cout<<"Enter the size of array" ;
+    cin >> n;
+
+    cout<<"Enter the elements of array" << endl;
+    for( int i=0 ; i<n ; i++ )
+    {
+        cin>>arr[i];
+    }
+
+    int ans;
+    ans = findDuplicate ( arr, n);
+
+    cout<<"The element that is repeating is : " << ans;
 }
